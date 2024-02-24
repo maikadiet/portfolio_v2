675af71ab37f20cf89cbccf3e4053dcaee2bc0f0
maikadie <73387131+maikadie@users.noreply.github.com>
2024-02-24 13:18:45 +0100

    Add project page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template lang="pug">
-  header
-    img(alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125")
-
-    div(class="wrapper")
-      HelloWorld(msg="You did it!")
-
-      nav
-        RouterLink(to="/") Home
-        RouterLink(to="/about") About
+  div
+    nav
+      RouterLink(to="/") Home
+      RouterLink(to="/about") About
   RouterView
 </template>
 <script setup>
