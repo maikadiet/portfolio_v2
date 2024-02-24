dfca7f2df71da9e7436221bb2ba3c29bb2047a20
maikadie <73387131+maikadie@users.noreply.github.com>
2024-02-24 14:38:08 +0100

    style nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
-  nav
-    RouterLink(to="/") Home
-    RouterLink(to="/about") About
+  ul(id="nav")
+    li
+      RouterLink(to="/") work
+    li ––
+    li
+      RouterLink(to="/about") about / contact
   RouterView
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 </script>
+
+<style scoped>
+#nav {
+  font-family: aktiv-grotesk-extended, sans-serif;
+  bottom: 1.25vw;
+  left: 2vw;
+  transform-origin: left;
+  transform: rotate(-90deg);
+  position: fixed;
+  color: black;
+}
+ul > li {
+  list-style-type: none;
+  margin-right: 10px;
+  padding: 0;
+  display: inline;
+}
+</style>
