7e8bc44fcfea366f29742aac3a1d66a207a14ea6
maikadie <73387131+maikadie@users.noreply.github.com>
2024-02-24 13:42:14 +0100

    clear vue default stuff

---
+++ src/components/Logo.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div Maika Dieterich
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  nav
+    RouterLink(to="/") Home
+    RouterLink(to="/about") About
+  RouterView
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
