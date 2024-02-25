d996a6ef4b02cb6975985423dad3497c96156bdf
maikadie <73387131+maikadie@users.noreply.github.com>
2024-02-25 10:47:29 +0100

    add projectData

---
+++ src/projectData.js
@@ -0,0 +1,94 @@
+export default {
+    projectdetails: [
+        /*{
+            title: "KITeGG",
+            types: "research | artificial intelligence | ongoing",
+            textde: "“Künstliche Intelligenz” gestaltet bereits heute unsere Gegenwart mit. Handy-Bilder werden automatisch mit Filtern versehen, unser Musik- und Filmgeschmack erkannt und geleitet, wir setzen auf Spracherkennungs- und Navigationsalgorithmen. Doch wie können KI-Modelle gezielt in den Gestaltungsprozess mit eingebunden werden? Und wann ist ihr Einsatz sinnvoll? Ob als gestalterisches Mittel, zur Prozessoptimierung oder Barrierefreiheit von Apps oder Websites, das gestalterische Potenzial ist beachtlich. Aber nicht nur das, Gestalter*innen, die sich mit der Technologie auskennen und sie kritisch hinterfragen, können auch den gesellschaftlichen Diskurs mitgestalten und an die Technologie heranführen. Kunst und Gestaltung können auf kritische Aspekte aufmerksam machen, spekulativ in die Zukunft schauen und überlegen, was Technologie leisten oder nicht leisten sollte.\n" +
+                "\nDas Verbundvorhaben „KITeGG - KI greifbar machen und begreifen: Technologie und Gesellschaft verbinden durch Gestaltung“ will genau diese Themen in die Gestaltungslehre bringen. Projektpartner sind die Hochschule Mainz, die Hochschule für Gestaltung Offenbach, die Hochschule für Gestaltung Schwäbisch Gmünd, die Köln International School of Design (TH Köln) und die Hochschule Trier.",
+            texten: "\"Artificial intelligence\" is already helping to shape our present. Mobile phone pictures are automatically supplied with filters, our taste in music and films is recognised and guided, we rely on voice recognition and navigation algorithms. But how can AI models be deliberately integrated into the design process? And when does their use make sense? Whether as a design tool, for process optimisation or accessibility of apps or websites, the design potential is considerable. But not only that, designers who are familiar with the technology and critically question it can also help shape the social discourse and introduce people to the technology. Art and design can draw attention to critical aspects, look speculatively into the future and consider what technology should or should not achieve.\n" +
+                "\nThe joint project \"KITeGG - Making AI tangible and comprehensible: Connecting technology and society through design\" aims to bring precisely these topics into design teaching. Project partners are the Mainz University of Applied Sciences, the Offenbach University of Art and Design, the Schwäbisch Gmünd University of Applied Design, the Cologne International School of Design and the Trier University of Applied Sciences.\n\n\n",
+            links: {"gestaltung.ai" : "https://gestaltung.ai/#/"},
+            images: [],
+            films: [],
+            id: "10"
+        },*/
+        {
+            title: "DesignUp",
+            types: "product design | upcycling | workshop",
+            textde: "DesignUp était une équipe d'environ 50 designers allemands et sénégalais, composée principalement d'artistes, de designers de produits/mode/industriel et d'artisans. Nous avons commencé à travailler ensemble numériquement en 2021 sur l'upcycling. En mai 2022, nous nous sommes réunis à Dakar. Les œuvres ont été exposées du 24 au 29 mai au \"Super Mbalit Festival\" dans le cadre du programme Off de la Biennale de Dak'Art.\n\nJ'ai travaillé avec Papis Diop (artiste et designer de produits). Nous avons conçu deux variantes de connexions en modelant des bouteilles en plastique à la chaleur, que nous avons présentées dans un cintre/sculpture. Avec Martin Bassène (spécialisé dans le travail du métal), j'ai conçu une lampe à partir de différents matériaux de récupération.\n\nPapis Diop, Sabine Boger (designer graphique et de produits) et moi avons donné un atelier pendant le festival, au cours duquel nous avons utilisé divers matériaux de rebut pour fabriquer des pinceaux avec les enfants de Yarrakh (lieu de l'exposition).\n\nPhotos de Sabine Boger et Maika Dieterich.",
+            texten: "DesignUp was a team of around 50 German and Senegalese Designers consisiting of mostly artists, product/fashion/industrial designers and artisans. We started working together digitally in 2021, focusing on upcycling. In May 2022 we got together in Dakar. The works were exhibited from the 24th – 29th of May in the “Super Mbalit Festival” as part of the Dak’Art Biennale Off programme.\n\nI worked with Papis Diop (artist and product designer). We designed two variations of screwless connections by modelling plastic bottles with heat, which we showcased in a coat hanger/sculpture. Together with Martin Bassène (specialised in metal work), I also designed a lamp out of different leftover materials.\n\nPapis Diop and Sabine Boger and I also gave a workshop during the festival, in which we used various rubbish materials to make paintbrushes with children of Yarrakh (the part of Dakar in which we had out exhibition).\n\nPhotos by Sabine Boger and Maika Dieterich.\n\n\n",
+            links: [],
+            images: ["designup_austellung.jpg","designup_baum_2.jpg", "designup_baum_lampe.jpg","designup_lampe.jpg","designup_lampe_3.jpg","designup_workshop.jpg","designup_workshop_1.jpg","designup_workshop_2.jpg"],
+            films: [],
+            id: "9"
+        },
+        {
+            title: "Hyperobjects",
+            types: "virtual reality | installation | interaction",
+            textde: "Hyperobjekte sind komplexe Systeme wie der Klimawandel oder das Internet. Diese Entitäten verteilen sich so massiv über Raum und Zeit, dass wir sie nur über Datenerhebungen wahrnehmen können. Mit fortschreitender Technisierung steigt auch die Menge Daten, die erhoben und ausgewertet werden können. Das bedeutet, dass auch die Anzahl, sowie die Komplexität der Hyperobjekte steigt, die wir entdecken. Dadurch gewinnt auch die Frage, wie Hyperobjekte angemessen kommuniziert können, so dass sensorisch und emotional Zugang zu ihnen geschaffen werden kann, gewinnt weiter an Relevanz.\n" +
+                "\nDas Thema wird in Form einer Virtual Reality Installation präsentiert, begleitet von einem Reader und einer Installationsfläche, auf der die Navigationselemente des virtuellen Raums in den realen Raum übersetzt werden. In den sechs Kapiteln der virtuellen Welt, können sich Betrachter*innen mit verschiedenen Aspekten der Philosophie über eingesprochenene Texte auseinandersetzen. Dazu sieht man verschiedene Animationen, die visuell den auditiven Input untermalen.",
+            texten: "Hyperobject is a term for complex systems such as climate change or the internet. Entities that extend massively across space and time, so that we humans cannot perceive them, but can only recognise them through data collection and evaluation. With digitalisation progressing worldwide, the amount of data we can collect and analyse grows. This in turn means that more and more complex hyperobjects are being discovered. Thus, the question of how hyperobjects can be communicated properly, once again allowing sensory and emotional engagement becomes more and more relevant. \n" +
+                "\nThe topic is presented in the form of a virtual reality application, as well as a reader and an installation space that translates the navigational elements of the virtual environment into the physical environment. In the virtual world, you can enter six different chapters, which explain different aspects of the topic in the form of spoken texts. In addition, one sees animations that visually support the auditive input.\n\n\n",
+            links: [],
+            images: ["hyperobjekte.png", "hyperobjekte_screenshot.jpg", "hyperobjekte_1.jpg", "hyperobjekte_reader.jpg"],
+            films: ["https://youtu.be/ckOFov81RD8"],
+            id: "8"
+        },
+        {
+            title: "Fingerprinting Language",
+            types: "creative coding | animation | layout",
+            textde: "Diese Arbeit ist ein Versuch, eine visuelle Form für die Ästhetik der Sprache zu finden. Grammatikalische Werte aus sieben verschiedenen Sprachen werden von dem Programm gelesen und verwendet, um das Aussehen und Verhalten jeder Animation zu bestimmen. Jedes Mal, wenn das Programm ausgeführt wird, entsteht ein einzigartiges Erscheinungsbild.\n" +
+                "\nSchreibrichtung, Anzahl der Zeichen im Alphabet und Zeitformen bestimmen das Verhalten, das Aussehen und die Anzahl der erzeugten Partikel. Mit den Ziffern auf der Tastatur können Sie zwischen sieben verschiedenen Sprachen umschalten. Das Programm lädt die entsprechenden Werte und erzeugt die Animation.\n" +
+                "\nDie Arbeit wurde als Teil der Still Here Austellung von PÄŤ & PÓL und der Bratislava Design Week Austellung im Winter 2022 in Bratislava, Slowakei ausgestellt.",
+            texten: "This work is an experiment on finding a visual form for the aesthetics of language. Grammatical values from seven different languages are read by the programme and used to determine the look and behaviour of each animation. Unique visuals are created each time the programme is run.\n" +
+                "\nDirection of writing, number of characters in the alphabet and tenses determine the behaviour, look and number of generated particles. With the digits on the keyboard you can switch between seven different languages. The program loads the corresponding values  and generates the animation.\n" +
+                "\nThe work was exhibited as part of the Still Here exhibition by PÄŤ & PÓL and as part of the exhibition of the Bratislava Design Week in Winter 2022 in Bratislava, Slovakia.\n\n\n",
+            links: [],
+            images: ["fingerprinting_1.jpg", "fingerprinting_2.jpg", "fingerprinting_3.jpg", "fingerprinting_4.jpg", "fingerprinting_5.jpg"],
+            films: ["https://youtu.be/rZivrrFZmuk"],
+            id: "7"
+        },
+        {
+            title: "Limbus",
+            types: "3D | virtual reality | webdesign | front-end",
+            textde:  "Limbus ist ein mehrdimensionales Browser- oder Cardboard VR Erlebnis, welches Hypothesen zu möglichen Zukunftsszenarien über die Einbettung künstlicher Intelligenz in unserer Gesellschaft aufstellt. Wie können Menschen und Technologie koexistieren? Wie könnte unsere Gesellschaft besten- oder schlimmsten Falls aussehen?\n" +
+                "Wir wollen euch dazu einladen, nicht nur die Zukunft moderner Technologie, sondern auch unsere gegenwärtige Beziehung zu Technologien zu reflektieren. Viel Spaß!\n" +
+                "\n" +
+                "Konzept und Design zusammen mit Deniz Tütüncübasi.\n" +
+                "Featured in FAYD DIGITAL ISSUE 001: Balance.",
+            texten: "Limbus is a multi dimensional browser or cardboard vr experience that hypothesises about the possible outcomes of implementing Artificial Intelligence in our society. How can humans and technology coexist? What could a best- or a worst-case scenario look like? \n" +
+                "We want to invite you to not only think about our future in regards to the usage of modern technology, but also reflect on our relationship to technological progress so far. Enjoy!\n" +
+                "\n" +
+                "Concept and design together with Deniz Tütüncübasi.\n" +
+                "Featured in FAYD DIGITAL ISSUE 001: Balance.\n\n\n",
+            links: {"Click here to enter Limbus" : "https://limbus-browser-version.glitch.me/"},
+            images: ["limbus.jpg", "limbus_1.png", "limbus_2.jpg", "limbus_3.png", "limbus_4.png"],
+            films: ["https://www.youtube.com/embed/ckOFov81RD8?si=rfWDgQHwG-HzQtd8"],
+            id: "6"
+        },
+        /*{
+            title: "Crypto Data Journalism",
+            types: "research | data journalism | freelance",
+            textde: "The Stack Report ist eine Publikation, die aus dem Tezos Ökosystem durch Visualisierung von Blockchain Daten berichtet. Dabei werden die Datenströme aus dem Tezos Stack von digitalen, sozialen und physischen Schichten der neuen, dezentralisierten Welt in Daten-getriebene wöchtenliche recaps, monatliche deep-dives und stories zu bestimmten Themen transformiert.\n" +
+                "\nIch war im Sommer 2022 Teil des Stack Report Teams und habe zwei deep-dive Artikel zu verschiedenen Themen rund um NFT Kunst beigetragen.",
+            texten: "The Stack Report is a publication covering stories in the Tezos ecosystem through blockchain data visualisations. It transforms the data streams coming from the Tezos stack of digital, social and physical layers of this new decentralised world into data-driven weekly recaps, monthly deep dives and long-form stories into specific themes and topics.\n" +
+                "\nI was part of the Stack Report team in the summer of 2022, contributing two deep-dive articles on different aspects of NFT Art.\n\n\n",
+            links: {"Link to first Article" : "https://thestackreport.xyz/articles/permissionless-generative-nft-minting-on-fx-hash-in-data"},
+            images: [],
+            films: [],
+            id: "5"
+        },
+        {
+            title: "Flipped",
+            types: "data visualisation | animation | 3D | social media",
+            textde: "Flipped – Moving Mobility.\n\nIn der globalisierten Welt des 21. Jahrhunderts werden Distanzen immer kürzer. Für uns ist es selbstverständlich geworden, dass wir überall hinkommen – und das mit relativ wenig Geld. Wir fragen uns, was die Konsequenzen einer so umfangreichen Mobilität sind und was sich verändern muss, damit sie nachhaltig und für alle nutzbar wird.\n" +
+                "\nZusammen mit meiner Kommilitonin Leonie Krein habe ich eine Plattform aufgebaut, die über alternative Arten der Mobilität informiert. Dabei gibt es Fakten in Form von animierten Infografiken und 3D Modellen, aber auch Meinungen von verschiedenen Menschen in Form von Interviews und Statements.",
+            texten: "Flipped – Moving Mobility.\n\nIn the globalised world of the 21st century, distances have become smaller than ever. Being able to go anywhere we want is a given for us – and for relatively little money at that. We ask ourselves what the consequences of such extensive mobility are and what could potentially change for it to become sustainable and available for everyone.\n" +
+                "\nTogether with my fellow student Leonie Krein we created a platform that informs about alternative forms of mobility in the form of animated infographics and 3D models, but also offers a space for opinions, for example by showing interviews and  statements on the topic.\n\n\n",
+            links: [],
+            images: ["flipped_mobility.png", "flipped_mobility_1.png", "flipped_mobility_film.gif"],
+            films: [],
+            id: "4"
+        }*/
+    ]
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
   Logo
+  img(
+    v-for="projectdetail in projectdetails",
+    :src="`@/assets/${projectdetail.images[0]}`",
+    :alt="projectdetail.title")
 </template>
 
 <script setup>
 import Logo from "@/components/Logo.vue";
+import projectData from "@/projectData.js";
+
+const projectdetails = projectData.projectdetails
+
 </script>
+
+<style>
+</style>
