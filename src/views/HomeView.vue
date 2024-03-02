ca3a4c69796ca8d4b48be4edb9f1c63f6e5171e7
maikadie <73387131+maikadie@users.noreply.github.com>
2024-03-02 16:40:03 +0100

    add routing to projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,32 @@
 <template lang="pug">
   Logo
-  img(
-    v-for="projectdetail in projectdetails",
-    :key="projectdetail.id",
-    :src="projectdetail.images[0]",
-    :alt="projectdetail.title")
+  figure(v-for="projectdetail in projectdetails",)
+    RouterLink(:to="{name: 'project', params: {id:projectdetail.id}}")
+      img.homeImg(
+        :key="projectdetail.id",
+        :src="projectdetail.images[0]",
+        :alt="projectdetail.title")
 </template>
 
 <script setup>
 import Logo from "@/components/Logo.vue";
 import projectData from "@/projectData.js";
+import {RouterLink} from "vue-router";
 
 
 const projectdetails = projectData.projectdetails
 
 </script>
 
-<style>
+<style scoped>
+.homeImg {
+  width: 50vw;
+  margin: 20vh 0 10vh 0;
+}
+@media screen and (max-width: 1025px) {
+  .homeImg {
+    margin: 10vh 0 10vh 10vw;
+    width: 75vw;
+  }
+}
 </style>
