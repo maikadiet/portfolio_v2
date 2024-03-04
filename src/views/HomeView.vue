82b186bb04aa17979b08d3bbf274bebff963afbc
maikadie <73387131+maikadie@users.noreply.github.com>
2024-03-04 09:27:26 +0100

    add moving type (jittery)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,68 @@
   figure(v-for="projectdetail in projectdetails",)
     RouterLink(:to="{name: 'project', params: {id:projectdetail.id}}")
       img.homeImg(
-        :key="projectdetail.id",
+        @mousemove="mouseMove",
+        @mouseenter="mouseEnter(projectdetail.id)",
+        @mouseleave="mouseLeave",
         :src="projectdetail.images[0]",
         :alt="projectdetail.title")
+  h1.movingType(
+    :id="'projectTitle'+projectdetail.id",
+    v-show="visible && currentId === projectdetail.id",
+    :style="{ top: titlePosition.y + 'px', left: titlePosition.x + 'px' }"
+  ) {{ projectdetail.title}}
 </template>
 
 <script setup>
-import Logo from "@/components/Logo.vue";
-import projectData from "@/projectData.js";
-import {RouterLink} from "vue-router";
-
+import Logo from "@/components/Logo.vue"
+import projectData from "@/projectData.js"
+import { RouterLink } from "vue-router"
+import { ref, reactive, onMounted, onUnmounted } from "vue"
+import { useMouse } from '@vueuse/core'
+const { x, y } = useMouse({ touch: false })
 
 const projectdetails = projectData.projectdetails
+const visible = ref(true)
+const currentId = ref(null)
+const titlePosition = reactive({ x: 0, y: 0 })
+let debounceTimer
+
+function debounce(func, delay) {
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(debounceTimer)
+    debounceTimer = setTimeout(() => func.apply(context, args), delay)
+  }
+}
+function mouseEnter(id) {
+  currentId.value = id
+  visible.value = true
+  titlePosition.x = x._rawValue
+  titlePosition.y = y._rawValue
+  window.addEventListener('mousemove', mouseMove, false)
+}
+function mouseLeave() {
+  visible.value = false
+  currentId.value = null
+}
+function mouseMove() {
+  debounce(() => {
+    if (currentId.value !== null) {
+      titlePosition.x = x.value;
+      titlePosition.y = y.value;
+    }
+  }, 1)(); // Adjust debounce delay as needed
+}
+
+onMounted(() => {
+  window.addEventListener('mousemove', mouseMove);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', mouseMove);
+})
+
 
 </script>
 
@@ -23,6 +73,13 @@ const projectdetails = projectData.projectdetails
   width: 50vw;
   margin: 20vh 0 10vh 0;
 }
+.movingType {
+  position: absolute;
+  font-family: 'LeMurmure';
+  font-size: 10vw;
+  color: black;
+}
+
 @media screen and (max-width: 1025px) {
   .homeImg {
     margin: 10vh 0 10vh 10vw;
